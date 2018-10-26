4152851f7a1f4d656afd9354cf77992cdb51d64a
mrabone94 <martinrabone94@gmail.com>
2018-10-26 15:34:55 +0100

    Initial Commit

---
+++ desktop/src/uk/ac/aston/team17/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package uk.ac.aston.team17.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import uk.ac.aston.team17.AstonAdventure;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AstonAdventure(), config);
+	}
+}
