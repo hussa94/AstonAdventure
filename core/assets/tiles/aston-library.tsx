2ccefa9ce29c4c43a513db93eb234a9638477de7
Martin Rabone <martinrabone94@gmail.com>
2019-02-25 18:18:21 +0000

    Updated with changes to buildings for Level One Map. Buildings are now tile sets allowing for collisions to be added

---
+++ core/assets/tiles/aston-library.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="aston-library" tilewidth="16" tileheight="16" tilecount="1456" columns="52">
+ <image source="aston-library.png" width="847" height="457"/>
+</tileset>
