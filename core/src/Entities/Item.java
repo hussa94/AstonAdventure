f1c1ebef3775d4004657720e7e222f0e28c27c09
jodieAsh <jodie_ash@hotmail.co.uk>
2019-03-05 09:04:30 +0000

    Added comments and JavaDoc to Item class. #16

--- core/src/Entities/Item.java
+++ core/src/Entities/Item.java
@@ -2,6 +2,9 @@ package Entities;
 
 import com.badlogic.gdx.graphics.Texture;
 
+/**
+ * Item class used to create a new items to be placed into the game.
+ */
 public class Item {
 
     //enum itemType of the item
@@ -15,7 +18,12 @@ public class Item {
 
     private Sounds Sm = new Sounds();
 
-    //TODO JavaDoc Constructor to create a new item
+    /**
+     * Item constructor is used to create a new item to insert into the game.
+     * @param itemType the enum {@link ItemType} of the item being created.
+     * @param xCoordinate the x coordinate of where the item will be placed on the game map.
+     * @param yCoordinate the y coordinate of where the item will be placed on the game map.
+     */
     public Item(ItemType itemType, int xCoordinate, int yCoordinate){
         this.itemType = itemType;
         this.xCoordinate = xCoordinate;
@@ -40,7 +48,12 @@ public class Item {
         return this.itemType;
     }
 
-    //TODO JavaDoc
+    /**
+     * A method to check whether or not the player is close enough to pick up the item.
+     * @param xPlayer the current x coordinate of the player on the game map.
+     * @param yPlayer the current y coordinate of the player on the game map.
+     * @return true if the item has successfully been picked up, false if the player is not near the item.
+     */
     public boolean isBeingPicked(float xPlayer, float yPlayer) {
         if (((xCoordinate - 20) < xPlayer && xPlayer < (xCoordinate + 20)) && ((yCoordinate - 20) < yPlayer && yPlayer < (yCoordinate + 20))) {
             if (!(Sm.isSoundPlaying())) {
