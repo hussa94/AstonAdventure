1b2ef7133f909bd977030b2b7cc79b19fe3fcb3b
Martin Rabone <martinrabone94@gmail.com>
2019-03-15 18:21:42 +0000

    Added music to level three #25

--- core/src/Screens/LevelThree.java
+++ core/src/Screens/LevelThree.java
@@ -71,7 +71,7 @@ public class LevelThree extends Level implements com.badlogic.gdx.Screen {
 
         //Background sounds
         if (!Sm.isMusicPlaying()) {
-            Sm.levelOne();
+            Sm.levelThree();
         }
 
         //Set view of camera
